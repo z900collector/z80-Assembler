4959bcb3619647b371de6900118d2f591916d40a
Sid Young <sid.young@gmail.com>
2026-09-23 09:41:52 +1000

    Reformated main and added more usage info

--- src/asm-v3.cpp
+++ src/asm-v3.cpp
@@ -1,12 +1,11 @@
 /*
- * z80 Assembler V3 using basic C++
+ * Source Code Assembler V3 using basic C++
+ * Defaults to Z80 instruction set but can do 6502 using the --6502 flag
  *
- * Compile cc file using:
- *
- * g++ -std=c++17 -Wall -Wextra -pedantic -Werror -g asm-v3.cc -o asm-v3
+ * Compile using ./build.sh
  *
  * Partly AI generated, manually edited and updated: S Young 2026
- * GITHUB Workflow added to project
+ * GITHUB Workflow added to project to do CI on push.
  *
  */
 
@@ -30,68 +29,68 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
-    {
-        std::cerr << "Usage: " << argv[0] << " <input.asm> [output.bin] [--6502]\n";
-        return 1;
-    }
+	if(argc < 2)
+	{
+		std::cerr << "Z80/6502 Assembler v3.0 Sid Young\n";
+		std::cerr << "defaults to Z80, use --6502 to compile different target architectures.\n\n";
+		std::cerr << "Usage: " << argv[0] << " <input.asm> [output.bin] [--6502]\n";
+		return 1;
+	}
 
-    std::string inputFile = argv[1];
-    std::string outputFile = "output.bin";
-    bool is6502 = false;
+	std::string inputFile = argv[1];
+	std::string outputFile = "output.bin";
+	bool is6502 = false;
 
-    // Parse arguments
-    for (int i = 2; i < argc; i++)
-    {
-        std::string arg = argv[i];
-        if (arg == "--6502")
-        {
-            is6502 = true;
-        }
-        else if (arg == "--z80")
-        {
-            is6502 = false;
-        }
-        else
-        {
-            outputFile = arg;
-        }
-    }
+	for(int i = 2; i < argc; i++)
+	{
+		std::string arg = argv[i];
+		if(arg == "--6502")
+		{
+			is6502 = true;
+		}
+		else if(arg == "--z80")
+		{
+			is6502 = false;
+		}
+		else
+		{
+			outputFile = arg;
+		}
+	}
 
-    std::ifstream in(inputFile);
-    if (!in)
-    {
-        std::cerr << "Cannot open input file: " << inputFile << "\n";
-        return 1;
-    }
+	std::ifstream in(inputFile);
+	if(!in)
+	{
+		std::cerr << "Cannot open input file: " << inputFile << "\n";
+		return 1;
+	}
 
-    std::stringstream buffer;
-    buffer << in.rdbuf();
-    std::string source = buffer.str();
+	std::stringstream buffer;
+	buffer << in.rdbuf();
+	std::string source = buffer.str();
 
-    // Select instruction set
-    std::unique_ptr<IInstructionSet> iset;
-    if (is6502)
-    {
-        iset = std::make_unique<MOS6502InstructionSet>();
-    }
-    else
-    {
-        iset = std::make_unique<Z80InstructionSet>();
-    }
+	// Select instruction set
+	std::unique_ptr<IInstructionSet> iset;
+	if(is6502)
+	{
+		iset = std::make_unique<MOS6502InstructionSet>();
+	}
+	else
+	{
+		iset = std::make_unique<Z80InstructionSet>();
+	}
 
-    Assembler assembler(*iset);
-    assembler.setOutputFile(outputFile);
-
-    try
-    {
-        assembler.assemble(source);
-    }
-    catch (const std::runtime_error& e)
-    {
-        std::cerr << "Assembly error: " << e.what() << "\n";
-        return 1;
-    }
-
-    return 0;
-}   
+	Assembler assembler(*iset);
+	assembler.setOutputFile(outputFile);
+	try
+	{
+		assembler.assemble(source);
+	}
+	catch(const std::runtime_error& e)
+	{
+		std::cerr << "Assembly error: " << e.what() << "\n";
+		return 1;
+	}
+	return 0;
+}
+// End of file
